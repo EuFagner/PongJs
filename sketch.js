ed7f4971855311ebadff622368e2d2a4cd87cbe5
Fagner <eufagne0@gmail.com>
2022-06-20 11:42:27 -0300

    feito bolinha principal

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+var xBolinha = 300;
+var yBolinha = 200;
+var diametro = 30;
+var raio = 30;
+
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
   background("black");
+  mostraBolinha();
+}
+
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro);
 }
