ab1c5f3ff6c0e60daf666983ec6bad121f63e89b
Fagner <eufagne0@gmail.com>
2022-06-20 13:56:52 -0300

    Minha Raquete

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+// Variáveis para a bolinha
 var xBolinha = 300;
 var yBolinha = 200;
 var diametro = 30;
-var raio = 30;
+var raio = diametro /2;
+
+// Velocidade da bolinha
+var velocidadeXBolinha = 8;
+var velocidadeYBolinha = 8;
+
+// Variáveis da raquete
+var xRaquete = 5;
+var yRaquete = 150;
+var comprimentoMinhaRaquete = 10;
+var alturaMinhaRaquete = 90; 
 
 function setup() {
   createCanvas(600, 400);
@@ -10,8 +21,31 @@ function setup() {
 function draw() {
   background("black");
   mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoComBorda();
+
+  mostraRaquete(xRaquete, yRaquete);
 }
 
 function mostraBolinha() {
   circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoComBorda() {
+  if(xBolinha + raio > width || xBolinha - raio < 0) {
+    velocidadeXBolinha *= -1;
+  }
+
+  if(yBolinha + raio > height || yBolinha - raio < 0) {
+    velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete(x, y) {
+  rect(x, y, comprimentoMinhaRaquete, alturaMinhaRaquete);
 }
